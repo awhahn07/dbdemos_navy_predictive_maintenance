bbfdbe99dc87fa82a16a973398a82f6ef0a7b60b
awhahn07 <awhahn07>
2025-10-03 15:23:13 +0000

    update DLT

--- 01-Data-Ingestion/01.1-DLT-Navy-Turbine-SQL.sql
+++ 01-Data-Ingestion/01.1-DLT-Navy-Turbine-SQL.sql
@@ -230,6 +230,7 @@ CREATE LIVE TABLE historical_sensor_silver (
 COMMENT "Hourly sensor stats, used to describe signal and detect anomalies"
 AS
 SELECT turbine_id,
+      abnormal_sensor, 
       date_trunc('hour', from_unixtime(timestamp)) AS hourly_timestamp, 
       avg(energy)          as avg_energy,
       stddev_pop(sensor_A) as std_sensor_A,
@@ -244,7 +245,7 @@ SELECT turbine_id,
       percentile_approx(sensor_D, array(0.1, 0.3, 0.6, 0.8, 0.95)) as percentiles_sensor_D,
       percentile_approx(sensor_E, array(0.1, 0.3, 0.6, 0.8, 0.95)) as percentiles_sensor_E,
       percentile_approx(sensor_F, array(0.1, 0.3, 0.6, 0.8, 0.95)) as percentiles_sensor_F
-  FROM historical_sensor_bronze GROUP BY hourly_timestamp, turbine_id
+  FROM historical_sensor_bronze GROUP BY hourly_timestamp, turbine_id, abnormal_sensor
 
 -- COMMAND ----------
 
